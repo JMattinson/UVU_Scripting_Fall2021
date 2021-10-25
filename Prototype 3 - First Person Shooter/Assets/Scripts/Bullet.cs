ee8f4e865f7e8b35aaba18c78a466493b99cb811
Jaden <jadenmattinson6@gmail.com>
2021-10-25 17:09:49 -0600

    Added basic health system for player & enemy

---
+++ Prototype 3 - First Person Shooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+    public float lifetime;
+    private float shootTime;
+
+    void OnEnable()
+    {
+         shootTime = Time.time;
+
+
+    }
+    void OnTriggerEnter(Collider other)//on collision with an object
+    {
+        if(other.CompareTag("Player")) //if target's player
+            other.GetComponent<playerController>().TakeDamage(damage);//apply damage
+
+        else if(other.CompareTag("Enemy"))//if target's enemy
+            other.GetComponent<EnemyAI>().TakeDamage(damage);//apply damage
+            
+        gameObject.SetActive(false);//disable self
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - shootTime >= lifetime)
+            gameObject.SetActive(false);
+    }
+
+}
